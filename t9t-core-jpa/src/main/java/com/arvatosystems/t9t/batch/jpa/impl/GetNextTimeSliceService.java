c56b7061eb22e1cb55c6121f544ce37607ed92aa
Michael Bischoff <Michael.Bischoff@bertelsmann.de>
2020-10-06 22:49:16 +0200

    improve timeSlice service

--- t9t-core-jpa/src/main/java/com/arvatosystems/t9t/batch/jpa/impl/GetNextTimeSliceService.java
+++ t9t-core-jpa/src/main/java/com/arvatosystems/t9t/batch/jpa/impl/GetNextTimeSliceService.java
@@ -24,31 +24,44 @@ public class GetNextTimeSliceService implements IGetNextTimeSliceService {
 
     @Override
     public SliceTrackingInterval getNextTimeSlice(RequestContext ctx, String dataSinkId, String id, Instant overrideAsOf, Long sinkRef) {
-        final Instant endInstantPrecise = overrideAsOf != null ? overrideAsOf : ctx.executionStart;
-        final Instant endInstantRoundedDown = endInstantPrecise.minus(endInstantPrecise.getMillis() % 1000L);
+        final Instant endInstantRoundedDown = ctx.executionStart.minus(ctx.executionStart.getMillis() % 1000L);
 
         final SliceTrackingInterval result = new SliceTrackingInterval();
         // we search for an existing timeSlice, if none is available, create a new one
         SliceTrackingEntity slice = resolver.findByDataSinkIdAndId(false, dataSinkId, id);
         if (slice == null) {
             result.setStartInstant(REALLY_LONG_AGO);
-            final Instant endInstantRoundedDownMinusGap = endInstantRoundedDown.minus(DEFAULT_GAP_IN_MILLIS);
             slice = resolver.newEntityInstance();
             slice.setDataSinkId(dataSinkId);
             slice.setId(id);
-            slice.setExportedDataBefore(endInstantRoundedDownMinusGap);
+            slice.setExportedDataBefore(overrideAsOf != null ? overrideAsOf : endInstantRoundedDown.minus(DEFAULT_GAP_IN_MILLIS));
             slice.setLastSinkRef(sinkRef);
             resolver.save(slice);
-            LOGGER.info("Creating NEW time slice for {}/{} to {}", dataSinkId, id, endInstantRoundedDownMinusGap);
+            LOGGER.info("Creating NEW time slice for {}/{} to {}", dataSinkId, id, slice.getExportedDataBefore());
         } else {
             result.setStartInstant(slice.getExportedDataBefore());
             final Integer gap = slice.getGap() != null ? slice.getGap() : DEFAULT_GAP_IN_SECONDS;
-            final Instant endInstantRoundedDownMinusGap = endInstantRoundedDown.minus(gap * 1000L);
-            LOGGER.debug("Advancing time slice for {}/{} from {} to {} (by {} seconds)", dataSinkId, id, slice.getExportedDataBefore(), endInstantRoundedDownMinusGap, gap);
-            slice.setExportedDataBefore(endInstantRoundedDownMinusGap);
+            slice.setExportedDataBefore(overrideAsOf != null ? overrideAsOf : endInstantRoundedDown.minus(gap * 1000L));
             slice.setLastSinkRef(sinkRef);
+            LOGGER.debug("Advancing time slice for {}/{} from {} to {} (by {} seconds)", dataSinkId, id, result.getStartInstant(), slice.getExportedDataBefore(), gap);
         }
         result.setEndInstant(slice.getExportedDataBefore());
+        return result;
+    }
+
+    @Override
+    public SliceTrackingInterval previewNextTimeSlice(RequestContext ctx, String dataSinkId, String id) {
+        final Instant endInstantRoundedDown = ctx.executionStart.minus(ctx.executionStart.getMillis() % 1000L);
+        final SliceTrackingInterval result = new SliceTrackingInterval();
+        final SliceTrackingEntity slice = resolver.findByDataSinkIdAndId(false, dataSinkId, id);
+        if (slice == null) {
+            result.setStartInstant(REALLY_LONG_AGO);
+            result.setEndInstant(endInstantRoundedDown.minus(DEFAULT_GAP_IN_MILLIS));
+        } else {
+            final Integer gap = slice.getGap() != null ? slice.getGap() : DEFAULT_GAP_IN_SECONDS;
+            result.setStartInstant(slice.getExportedDataBefore());
+            result.setEndInstant(endInstantRoundedDown.minus(gap * 1000L));
+        }
         return result;
     }
 }
