c6156152475e6c507fb0f17039f97dcc074a5997
Michael Bischoff <Michael.Bischoff@Bertelsmann.de>
2022-01-10 14:13:03 +0100

    fix removed package header

--- t9t-doc-be/src/main/java/com/arvatosystems/t9t/barcode/be/impl/BarcodeCode128Generator.java
+++ t9t-doc-be/src/main/java/com/arvatosystems/t9t/barcode/be/impl/BarcodeCode128Generator.java
@@ -13,6 +13,8 @@
  * See the License for the specific language governing permissions and
  * limitations under the License.
  */
+package com.arvatosystems.t9t.barcode.be.impl;
+
 import com.arvatosystems.t9t.doc.services.ImageParameter;
 import com.arvatosystems.t9t.image.be.impl.AbstractImageGenerator;
 import com.google.zxing.BarcodeFormat;
